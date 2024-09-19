234dd94886d9427c01ca73f30b3bd332daae3742
hyewo <hyewo@hyewon.cencol.ca>
2024-09-19 10:56:18 -0400

    first commit

---
+++ src/main/java/com/hh/assignment1/view/HyewonComp303Assignment1Application.java
@@ -0,0 +1,13 @@
+package com.hh.assignment1.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyewonComp303Assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyewonComp303Assignment1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hh/assignment1/springmvc/HyewonComp303Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hh.assignment1.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyewonComp303Assignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
